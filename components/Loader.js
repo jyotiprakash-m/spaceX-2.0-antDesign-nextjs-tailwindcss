396e1bf852d6672dc8672884ff2629c63ac278fd
jyotiprakash-m <teachmeonline365@gmail.com>
2021-07-04 18:18:17 +0530

    improve image

--- components/Loader.js
+++ components/Loader.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Loader() {
     return (
         <div className="w-full h-screen flex justify-center">
-            <Image className="" alt="loader" src="https://i.pinimg.com/originals/a7/12/3a/a7123a124ba35c74c421e1678e2bb677.gif" />
+            <Image preview={false} className="" alt="loader" src="https://i.pinimg.com/originals/a7/12/3a/a7123a124ba35c74c421e1678e2bb677.gif" />
         </div>
     )
 }
